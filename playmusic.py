ac4c205ce7bfd9012e2ae39affaaf8dc341d91fc
Adriano Santos <54894816+adrianosantosgit@users.noreply.github.com>
2024-02-27 21:36:14 -0300

    Create playmusic.py

---
+++ playmusic.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('music.mp3')
+
+# Play the music
+pygame.mixer.music.play()
+
+pygame.event.wait()
+# Keep the program running until the user closes the window or presses a key
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)  # Adjust the tick rate as needed
+
+# Clean up resources
+pygame.quit()
